b9fe11bfec0ce7a819eb18e471b2ee7c1f7c3e07
Vitor Mattos <vitor@php.rio>
2026-09-14 00:38:57 -0300

    feat: register sitemap listener

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// SPDX-FileCopyrightText: 2026 Vitor Mattos
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/** @var $events \TightenCo\Jigsaw\Events\EventBus */
+$events->afterBuild(App\Listeners\GenerateSitemap::class);
