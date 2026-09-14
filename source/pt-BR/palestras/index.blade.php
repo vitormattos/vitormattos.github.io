a62a002a182939659870ed93d2335d67ac268438
Vitor Mattos <vitor@php.rio>
2026-09-14 01:47:54 -0300

    feat: present Portuguese talks as reusable slide cards

--- source/pt-BR/palestras/index.blade.php
+++ source/pt-BR/palestras/index.blade.php
@@ -9,6 +9,14 @@ description: Palestras e apresentações de Vitor Mattos sobre software livre, P
 {{-- SPDX-License-Identifier: AGPL-3.0-or-later --}}
 @extends('_layouts.main')
 
+@push('head')
+    <link rel="stylesheet" href="{{ $page->baseUrl }}{{ vite('source/_assets/scss/presentations.scss') }}">
+@endpush
+
+@push('scripts')
+    <script type="module" src="{{ $page->baseUrl }}{{ vite('source/_assets/js/presentations.js') }}"></script>
+@endpush
+
 @section('body')
 <section class="content" aria-labelledby="page-title">
     <header>
@@ -17,12 +25,10 @@ description: Palestras e apresentações de Vitor Mattos sobre software livre, P
         <p class="lead">Palestras e apresentações sobre software livre, PHP, LibreSign, engenharia de software e construção de comunidades.</p>
     </header>
 
-    @foreach ($talks as $talk)
-        <article>
-            <p class="meta"><time datetime="{{ date('Y-m-d', $talk->date) }}">{{ date('d/m/Y', $talk->date) }}</time></p>
-            <h2><a href="{{ $talk->getUrl() }}">{{ $talk->title }}</a></h2>
-            <p>{{ $talk->description }}</p>
-        </article>
-    @endforeach
+    <div class="talk-list">
+        @foreach ($talks as $talk)
+            @include('_partials.talk.card', ['talk' => $talk])
+        @endforeach
+    </div>
 </section>
 @endsection
