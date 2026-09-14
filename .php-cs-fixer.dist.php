929042ef2765fb8e62eda89063cd4524d71aa9c5
Vitor Mattos <vitor@php.rio>
2026-09-14 00:16:10 -0300

    fix: align risky fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 // SPDX-FileCopyrightText: 2026 Vitor Mattos
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
@@ -8,7 +10,7 @@ $finder = PhpCsFixer\Finder::create()
     ->exclude(['build_local', 'build_production', 'vendor']);
 
 return (new PhpCsFixer\Config())
-    ->setRiskyAllowed(false)
+    ->setRiskyAllowed(true)
     ->setRules([
         '@PER-CS2.0' => true,
         'declare_strict_types' => true,
