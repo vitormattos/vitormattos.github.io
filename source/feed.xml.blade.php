9c00a38d7f2e04dcb30f7e14611ce93d28f6bba6
Vitor Mattos <vitor@php.rio>
2026-09-14 01:24:34 -0300

    seo: add English RSS feed

---
+++ source/feed.xml.blade.php
@@ -0,0 +1,35 @@
+---
+permalink: /feed.xml
+---
+{{-- SPDX-FileCopyrightText: 2026 Vitor Mattos --}}
+{{-- SPDX-License-Identifier: AGPL-3.0-or-later --}}
+@php
+    $siteUrl = rtrim($page->siteUrl, '/');
+    $latestDate = null;
+    foreach ($articlesEn as $article) {
+        $latestDate ??= $article->date;
+    }
+@endphp
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+    <title>{{ $page->siteName }}</title>
+    <link>{{ $siteUrl }}/</link>
+    <description>{{ $page->siteDescription }}</description>
+    <language>en</language>
+    <atom:link href="{{ $siteUrl }}/feed.xml" rel="self" type="application/rss+xml" />
+    @if ($latestDate)
+        <lastBuildDate>{{ date(DATE_RSS, $latestDate) }}</lastBuildDate>
+    @endif
+    @foreach ($articlesEn as $article)
+        @php($url = $siteUrl . '/' . ltrim($article->getPath(), '/'))
+        <item>
+            <title>{{ $article->title }}</title>
+            <link>{{ $url }}</link>
+            <guid isPermaLink="true">{{ $url }}</guid>
+            <pubDate>{{ date(DATE_RSS, $article->date) }}</pubDate>
+            <description>{{ $article->description }}</description>
+        </item>
+    @endforeach
+</channel>
+</rss>
