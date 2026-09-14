5d7cb18dc4bd3baaa9f275da7d32b71753b9bb81
Vitor Mattos <vitor@php.rio>
2026-09-14 00:16:31 -0300

    test: cover localized site output

--- tests/SiteBuildTest.php
+++ tests/SiteBuildTest.php
@@ -14,5 +14,21 @@ final class SiteBuildTest extends TestCase
         self::assertFileExists(__DIR__ . '/../build_production/index.html');
         self::assertFileExists(__DIR__ . '/../build_production/artigos/primeiro-artigo/index.html');
         self::assertFileExists(__DIR__ . '/../build_production/palestras/software-livre/index.html');
+        self::assertFileExists(__DIR__ . '/../build_production/en/index.html');
+        self::assertFileExists(__DIR__ . '/../build_production/en/articles/first-article/index.html');
+        self::assertFileExists(__DIR__ . '/../build_production/en/talks/free-software/index.html');
+    }
+
+    public function testLocalizedPagesExposeCorrectLanguage(): void
+    {
+        $portuguese = file_get_contents(__DIR__ . '/../build_production/index.html');
+        $english = file_get_contents(__DIR__ . '/../build_production/en/index.html');
+
+        self::assertIsString($portuguese);
+        self::assertIsString($english);
+        self::assertStringContainsString('<html lang="pt-BR">', $portuguese);
+        self::assertStringContainsString('<html lang="en">', $english);
+        self::assertStringContainsString('hreflang="en"', $portuguese);
+        self::assertStringContainsString('hreflang="pt-BR"', $english);
     }
 }
