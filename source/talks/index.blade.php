624c930d90d9f55567d9e330aa0af6c534e4eec1
Vitor Mattos <vitor@php.rio>
2026-09-14 10:29:27 -0300

    feat: add grid and list controls to talks

--- source/talks/index.blade.php
+++ source/talks/index.blade.php
@@ -12,7 +12,6 @@ description: Talks and presentations by Vitor Mattos about free software, PHP, L
 @push('head')
     <link rel="stylesheet" href="{{ $page->baseUrl }}{{ vite('source/_assets/scss/presentations.scss') }}">
 @endpush
-
 @push('scripts')
     <script type="module" src="{{ $page->baseUrl }}{{ vite('source/_assets/js/presentations.js') }}"></script>
 @endpush
@@ -24,8 +23,13 @@ description: Talks and presentations by Vitor Mattos about free software, PHP, L
         <h1 id="page-title">Talks</h1>
         <p class="lead">Talks and presentations about free software, PHP, LibreSign, software engineering and community building.</p>
     </header>
-
-    <div class="talk-list">
+    <div class="gallery-toolbar">
+        <div class="gallery-switcher" data-gallery-switcher aria-label="Presentation layout">
+            <button type="button" data-gallery-view="grid" aria-pressed="true">Grid</button>
+            <button type="button" data-gallery-view="list" aria-pressed="false">List</button>
+        </div>
+    </div>
+    <div class="talk-list" data-talk-gallery data-view="grid">
         @foreach ($talksEn as $talk)
             @include('_partials.talk.card', ['talk' => $talk])
         @endforeach
