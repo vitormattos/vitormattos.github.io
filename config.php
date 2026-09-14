f7a372f16a48d1ba65ccb361431dead9721eb221
Vitor Mattos <vitor@php.rio>
2026-09-14 00:38:10 -0300

    feat: define indexing policy by environment

--- config.php
+++ config.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 return [
     'baseUrl' => '',
     'production' => false,
+    'indexable' => false,
     'siteName' => 'Vitor Mattos',
     'defaultLocale' => 'en',
     'locales' => ['en', 'pt-BR'],
