9965f5450b321cb74ebe51a2796133769beb45f7
Vitor Mattos <vitor@php.rio>
2026-09-15 20:46:45 -0300

    test: derive topic counts from rendered cards

--- tests/TalkCatalogBuildTest.php
+++ tests/TalkCatalogBuildTest.php
@@ -24,13 +24,28 @@ final class TalkCatalogBuildTest extends TestCase
         self::assertGreaterThanOrEqual(18, substr_count($html, 'class="talk-card"'));
     }
 
-    public function testTagTaxonomyIsCaseInsensitive(): void
+    public function testTopicTaxonomyIsCaseInsensitiveAndCountsRenderedCards(): void
     {
         $html = file_get_contents($this->buildDirectory() . '/pt-BR/palestras/index.html');
         self::assertIsString($html);
 
         self::assertSame(1, substr_count($html, 'data-talk-tag="php"'));
         self::assertSame(0, substr_count($html, 'data-talk-tag="PHP"'));
-        self::assertMatchesRegularExpression('/data-talk-tag="php"><span>PHP<\/span><span>10<\/span>/', $html);
+
+        preg_match('/data-talk-tag="php"><span>[^<]+<\/span><span>(\d+)<\/span>/', $html, $counterMatch);
+        self::assertArrayHasKey(1, $counterMatch);
+        $displayedCount = (int) $counterMatch[1];
+
+        preg_match_all('/<article class="talk-card" data-talk-tags="([^"]*)">/', $html, $cardMatches);
+        $cardsWithPhp = 0;
+        foreach ($cardMatches[1] as $encodedTopics) {
+            $topics = json_decode(html_entity_decode($encodedTopics, ENT_QUOTES | ENT_HTML5, 'UTF-8'), true);
+            if (is_array($topics) && in_array('php', $topics, true)) {
+                ++$cardsWithPhp;
+            }
+        }
+
+        self::assertSame($cardsWithPhp, $displayedCount);
+        self::assertGreaterThan(0, $displayedCount);
     }
 }
