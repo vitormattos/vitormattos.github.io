f0bfb0bb2cf434d221d72c4f420436ec8465ee96
Vitor Mattos <vitor@php.rio>
2026-09-16 18:53:16 -0300

    style: format author profile template

--- source/_partials/author/profile.blade.php
+++ source/_partials/author/profile.blade.php
@@ -2,9 +2,8 @@
 {{-- SPDX-License-Identifier: AGPL-3.0-or-later --}}
 @php
     $profileLocale = $locale ?? ($page->locale ?? $page->defaultLocale);
-    $profileSummary = $page->author['summary'][$profileLocale]
-        ?? $page->author['summary'][$page->defaultLocale]
-        ?? null;
+    $profileSummary =
+        $page->author['summary'][$profileLocale] ?? ($page->author['summary'][$page->defaultLocale] ?? null);
 @endphp
 <div class="talks-profile">
     <img class="talks-profile__avatar" src="{{ $page->author['avatar'] }}" alt="" width="56" height="56">
