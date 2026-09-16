e2e41fb9d6efdd63354bb37f54a0826de7f04863
Vitor Mattos <vitor@php.rio>
2026-09-16 08:05:48 -0300

    feat: add curated talk history metadata

---
+++ data/talk-history.php
@@ -0,0 +1,120 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 Vitor Mattos
+// SPDX-License-Identifier: CC-BY-SA-4.0
+
+declare(strict_types=1);
+
+return [
+    'https://slides.com/vitormattos/contribuindo-e-criando-software-livre' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/12',
+        'recordings' => [
+            ['label' => 'FISL 2018', 'url' => 'http://hemingway.softwarelivre.org/fisl18/high/Sala%203/sala3-high-201807141605.ogv'],
+        ],
+        'alternateSlides' => [
+            ['label' => 'SlideShare', 'url' => 'https://www.slideshare.net/vitormattosrj/contribuindo-e-criando-software-livre'],
+        ],
+    ],
+    'https://slides.com/vitormattos/que-as-oportunidades-me-encontrem' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/17',
+        'recordings' => [
+            ['label' => 'TDC Floripa 2017', 'url' => 'https://en.eventials.com/Globalcode/que-oportunidades-me-encontrem-carreira-intel-vitor-mattos/'],
+            ['label' => 'FISL 2018', 'url' => 'http://hemingway.softwarelivre.org/fisl18/high/Sala%202/sala2-high-201807141704.ogv'],
+        ],
+        'alternateSlides' => [
+            ['label' => 'SlideShare', 'url' => 'https://pt.slideshare.net/vitormattosrj/que-as-oportunidades-me-encontrem-75733906'],
+        ],
+    ],
+    'https://slides.com/vitormattos/arquitetura-de-aplicacoes-web' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/42',
+        'appearances' => [
+            [
+                'event' => 'Ecossistema de aplicações web',
+                'date' => '2018-07-05',
+                'venue' => 'CO.W. Coworking Space',
+                'url' => 'https://www.sympla.com.br/palestra-ecossitema-de-aplicacoes-web__306779',
+            ],
+        ],
+    ],
+    'https://slides.com/vitormattos/seguranca' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/56',
+    ],
+    'https://slides.com/vitormattos/senhas' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/57',
+        'recordings' => [
+            ['label' => 'Webinar', 'url' => 'https://www.youtube.com/watch?v=4LlVcDot94s'],
+        ],
+        'alternateSlides' => [
+            ['label' => 'Webinar slides', 'url' => 'https://slides.com/vitormattos/webinar-senhas#/'],
+            ['label' => 'SlideShare', 'url' => 'https://www.slideshare.net/vitormattosrj/senhas-e-chaveiros-incremente-sua-seguranca'],
+        ],
+    ],
+    'https://slides.com/vitormattos/trabalho-colaborativo' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/71',
+    ],
+    'https://slides.com/vitormattos/data-detox' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/89',
+        'appearances' => [
+            [
+                'event' => 'Cerveja com Dados RJ #05',
+                'url' => 'https://www.meetup.com/Cerveja-com-Dados/events/260251990/',
+            ],
+        ],
+    ],
+    'https://slides.com/vitormattos/nextcloud' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/108',
+        'recordings' => [
+            ['label' => 'Video', 'url' => 'https://youtu.be/1Ofuy9CMDXw'],
+        ],
+    ],
+    'https://slides.com/vitormattos/webscraping' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/109',
+        'recordings' => [
+            ['label' => 'Video', 'url' => 'https://www.youtube.com/watch?v=zo4tXW74a4k'],
+        ],
+        'resources' => [
+            ['label' => 'Exercises', 'url' => 'https://github.com/vitormattos/exemplo-web-Scraping'],
+        ],
+    ],
+    'https://slides.com/vitormattos/celular_floss' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/126',
+        'recordings' => [
+            ['label' => 'Video', 'url' => 'https://youtu.be/43hFB-Jw2TE'],
+        ],
+    ],
+    'https://slides.com/vitormattos/libresign' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/131',
+        'recordings' => [
+            ['label' => 'Video', 'url' => 'https://youtu.be/WJpe_NnmW8o'],
+        ],
+    ],
+    'https://slides.com/vitormattos/cloud-privacity' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/146',
+        'recordings' => [
+            ['label' => 'Video', 'url' => 'https://www.youtube.com/watch?v=h2UF0h70NTA'],
+        ],
+        'appearances' => [
+            [
+                'event' => 'PHPRio',
+                'date' => '2022-08-03',
+                'venue' => 'CICC - Centro Integrado de Comando e Controle, Secretaria de Estado de Polícia Militar',
+                'url' => 'https://www.meetup.com/pt-BR/php-rio/events/287427112/',
+            ],
+        ],
+    ],
+    'https://slides.com/vitormattos/testes-api' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/172',
+        'resources' => [
+            ['label' => 'Example code', 'url' => 'https://github.com/vitormattos/api-test/'],
+        ],
+    ],
+    'https://slides.com/vitormattos/docker-dev-to-prod' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/173',
+    ],
+    'https://slides.com/vitormattos/nextcloud-app' => [
+        'cfp' => 'https://github.com/PHPRio/CFP/issues/152',
+        'resources' => [
+            ['label' => 'Development environment', 'url' => 'https://github.com/LibreCodeCoop/nextcloud-docker-development'],
+        ],
+    ],
+];
