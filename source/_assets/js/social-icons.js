0ef9c8454bf2605907f9994a8be244efaef70f0b
Vitor Mattos <vitor@php.rio>
2026-09-16 20:18:19 -0300

    feat: render RSS icon with Font Awesome

--- source/_assets/js/social-icons.js
+++ source/_assets/js/social-icons.js
@@ -2,18 +2,30 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faRss } from '@fortawesome/free-solid-svg-icons';
 
-const icons = {
+const brandIcons = {
     github: faGithub,
     linkedin: faLinkedin,
 };
 
-document.querySelectorAll('[data-brand-icon]').forEach((container) => {
-    const icon = icons[container.dataset.brandIcon];
+const uiIcons = {
+    rss: faRss,
+};
+
+function renderIcon(container, icon) {
     if (!icon) {
         return;
     }
 
     const [width, height, , , path] = icon.icon;
     container.innerHTML = `<svg viewBox="0 0 ${width} ${height}" aria-hidden="true" focusable="false"><path fill="currentColor" d="${path}"></path></svg>`;
+}
+
+document.querySelectorAll('[data-brand-icon]').forEach((container) => {
+    renderIcon(container, brandIcons[container.dataset.brandIcon]);
+});
+
+document.querySelectorAll('[data-ui-icon]').forEach((container) => {
+    renderIcon(container, uiIcons[container.dataset.uiIcon]);
 });
