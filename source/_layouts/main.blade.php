1e499c91fde0f7022613533136e430cf9232e2c3
Vitor Mattos <vitor@php.rio>
2026-09-16 19:03:07 -0300

    feat: load bundled social icons

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -89,6 +89,7 @@
         </p>
         <p><a href="{{ rtrim($page->siteUrl, '/') }}{{ $isEnglish ? '/feed.xml' : '/pt-BR/feed.xml' }}">RSS</a></p>
     </footer>
+    <script type="module" src="{{ $page->baseUrl }}{{ vite('source/_assets/js/social-icons.js') }}"></script>
     @stack('scripts')
 </body>
 
