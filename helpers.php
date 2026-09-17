ecafc70c4a2c0ee4750ec18a307a0433492220ca
Vitor Mattos <vitor@php.rio>
2026-09-16 21:50:25 -0300

    refactor: wire SEO services explicitly

--- helpers.php
+++ helpers.php
@@ -6,10 +6,17 @@
 declare(strict_types=1);
 
 use App\Presentations\PresentationThumbnailResolver;
+use App\Seo\PageUrlResolver;
 use App\Seo\SeoMetadataBuilder;
+use App\Seo\SocialImageResolver;
+use App\Seo\StructuredDataBuilder;
 
 $thumbnailResolver = new PresentationThumbnailResolver(__DIR__);
-$seoMetadataBuilder = new SeoMetadataBuilder($thumbnailResolver);
+$seoMetadataBuilder = new SeoMetadataBuilder(
+    new PageUrlResolver(),
+    new SocialImageResolver($thumbnailResolver),
+    new StructuredDataBuilder(),
+);
 
 return [
     'presentationThumbnail' => static function ($page, object $item) use ($thumbnailResolver): ?array {
