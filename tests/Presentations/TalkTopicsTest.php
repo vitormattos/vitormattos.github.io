c1d602681770553a5e15e40f3ea60dd4f5b9e4fe
Vitor Mattos <vitor@php.rio>
2026-09-17 01:31:29 -0300

    test: cover related technology taxonomy expansion

--- tests/Presentations/TalkTopicsTest.php
+++ tests/Presentations/TalkTopicsTest.php
@@ -52,6 +52,48 @@ final class TalkTopicsTest extends TestCase
         self::assertArrayNotHasKey('opensource', $topics);
     }
 
+    public function testRelatedTechnologyTopicsAreExpandedTransitively(): void
+    {
+        $talk = (object) ['tags' => ['LibreSign']];
+
+        $topics = TalkTopics::resolve($talk);
+
+        self::assertSame(
+            ['libresign', 'nextcloud', 'php'],
+            array_keys($topics),
+        );
+        self::assertSame('Nextcloud', $topics['nextcloud']);
+        self::assertSame('PHP', $topics['php']);
+    }
+
+    public function testPhpProjectsAreDiscoverableUnderPhp(): void
+    {
+        foreach (['e-Cidade', 'i-Educar', 'Nextcloud', 'MediaWiki', 'GLPI'] as $project) {
+            $topics = TalkTopics::resolve((object) ['tags' => [$project]]);
+
+            self::assertArrayHasKey('php', $topics, $project . ' should imply PHP');
+            self::assertSame('PHP', $topics['php']);
+        }
+    }
+
+    public function testLibreSignECidadePresentationExpandsToNextcloudAndPhp(): void
+    {
+        $talk = (object) [
+            'slidesId' => 3238741,
+            'presentation' => [
+                'type' => 'slides.com',
+                'metadata' => '/presentations/slides.com/3238741/metadata.json',
+            ],
+        ];
+
+        $topics = TalkTopics::resolve($talk);
+
+        self::assertArrayHasKey('libresign', $topics);
+        self::assertArrayHasKey('e-cidade', $topics);
+        self::assertArrayHasKey('nextcloud', $topics);
+        self::assertArrayHasKey('php', $topics);
+    }
+
     public function testTaxonomyCountsEachTalkOncePerNormalizedTopicAndExposesOnlyRecurringTopics(): void
     {
         $talks = [
