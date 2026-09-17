fa61afb7266574948017bf149cce155e662b514d
Vitor Mattos <vitor@php.rio>
2026-09-17 01:01:40 -0300

    Load talk list flow stylesheet in Portuguese catalog

--- source/pt-BR/palestras/index.blade.php
+++ source/pt-BR/palestras/index.blade.php
@@ -12,6 +12,7 @@ description: Palestras e apresentações de Vitor Mattos sobre software livre, P
 @push('head')
     <link rel="stylesheet" href="{{ $page->baseUrl }}{{ vite('source/_assets/scss/presentations.scss') }}">
     <link rel="stylesheet" href="{{ $page->baseUrl }}{{ vite('source/_assets/scss/talks-catalog.scss') }}">
+    <link rel="stylesheet" href="{{ $page->baseUrl }}{{ vite('source/_assets/scss/talks-list-flow.scss') }}">
 @endpush
 @push('scripts')
     <script type="module" src="{{ $page->baseUrl }}{{ vite('source/_assets/js/presentations.js') }}"></script>
